9bc7a6d116012fbb78493adf920ea94499306ae7
Liwt <19781209@qq.com>
2025-10-07 14:22:38 +0800

    fix

--- EIMSNext/Service/EIMSNext.ApiService/RequestModel/RequestBase.cs
+++ EIMSNext/Service/EIMSNext.ApiService/RequestModel/RequestBase.cs
@@ -1,9 +1,12 @@
-﻿using EIMSNext.Core.Entity;
+﻿using System.Text.Json.Serialization;
+using EIMSNext.Common;
+using EIMSNext.Core.Entity;
 
 namespace EIMSNext.ApiService.RequestModel
 {
     public abstract class RequestBase : IMongoEntity
     {
+        [JsonPropertyName(Constants.Field_BsonId)]
         public string Id { get; set; } = "";
     }
 }
