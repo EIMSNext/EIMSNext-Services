fb70c1f1d018ba1591a4c82466fa68756e134915
Liwt <19781209@qq.com>
2026-01-03 22:27:25 +0800

    表单发布

--- EIMSNext/Service/EIMSNext.ApiService/RequestModel/AuthGroupRequest.cs
+++ EIMSNext/Service/EIMSNext.ApiService/RequestModel/AuthGroupRequest.cs
@@ -1,7 +1,46 @@
-﻿namespace EIMSNext.ApiService.RequestModel
+﻿using EIMSNext.Entity;
+
+namespace EIMSNext.ApiService.RequestModel
 {
     public class AuthGroupRequest : RequestBase
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AppId { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FormId { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Desc { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public AuthGroupType Type { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<Member> Members { get; set; } = new List<Member>();
+        /// <summary>
+        /// 
+        /// </summary>
+        public DataPerms DataPerms { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? DataFilter { get; set; }
+        /// <summary>
+        /// 是否禁用
+        /// </summary>
+        public bool Disabled { get; set; }
     }
 }
 
--- EIMSNext/Service/EIMSNext.Entity/AuthGroup.cs
+++ EIMSNext/Service/EIMSNext.Entity/AuthGroup.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using EIMSNext.Core.Entity;
+﻿using EIMSNext.Core.Entity;
 
 namespace EIMSNext.Entity
 {
@@ -24,6 +18,159 @@ namespace EIMSNext.Entity
         /// <summary>
         /// 
         /// </summary>
-        public string Name {  get; set; } = string.Empty;   
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Desc { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public AuthGroupType Type { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<Member> Members
+        { get; set; } = new List<Member>();
+        /// <summary>
+        /// 
+        /// </summary>
+        public DataPerms DataPerms { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? DataFilter { get; set; }
+        /// <summary>
+        /// 是否禁用
+        /// </summary>
+        public bool Disabled { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum AuthGroupType
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        ManageSelfData = 0,
+        /// <summary>
+        /// 
+        /// </summary>
+        ViewAllData = 1,
+        /// <summary>
+        /// 
+        /// </summary>
+        ManageAllData = 2,
+        /// <summary>
+        /// 
+        /// </summary>
+        Custom = 3,
+    }
+
+    /// <summary>
+    /// 成员对象
+    /// </summary>
+    public class Member
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? Code { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Label { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public MemberType Type { get; set; }
+    }
+    /// <summary>
+    /// 成员类型，与前端的 SelectedTag组件的TagType对应
+    /// </summary>
+    public enum MemberType
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        NotSet = 0,
+        /// <summary>
+        /// 
+        /// </summary>
+        Department,
+        /// <summary>
+        /// 
+        /// </summary>
+        Employee,
+        /// <summary>
+        /// 
+        /// </summary>
+        Role,
+    }
+
+    /// <summary>
+    /// 数据权限
+    /// </summary>
+    [Flags]
+    public enum DataPerms : long
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// 
+        /// </summary>
+        View = 1 << 0,
+        /// <summary>
+        /// 
+        /// </summary>
+        AddNew = 1 << 1,
+        /// <summary>
+        /// 
+        /// </summary>
+        Edit = 1 << 2,
+        /// <summary>
+        /// 
+        /// </summary>
+        Copy = 1 << 3,
+        /// <summary>
+        /// 
+        /// </summary>
+        Remove = 1 << 4,
+        /// <summary>
+        /// 
+        /// </summary>
+        Import = 1 << 5,
+        /// <summary>
+        /// 
+        /// </summary>
+        Export = 1 << 6,
+    }
+
+    /// <summary>
+    /// 字段权限
+    /// </summary>
+    public class FieldPerm
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Visible { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Editable { get; set; }
     }
 }
