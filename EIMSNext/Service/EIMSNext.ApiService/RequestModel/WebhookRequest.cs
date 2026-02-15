713de77c92aba7562bc73bd7a51db99985b40428
Liwt <19781209@qq.com>
2026-02-16 01:18:54 +0800

    数据推送

--- EIMSNext/Service/EIMSNext.ApiService/RequestModel/WebhookRequest.cs
+++ EIMSNext/Service/EIMSNext.ApiService/RequestModel/WebhookRequest.cs
@@ -1,7 +1,31 @@
-﻿namespace EIMSNext.ApiService.RequestModel
+﻿using EIMSNext.Entity;
+
+namespace EIMSNext.ApiService.RequestModel
 {
     public class WebhookRequest : RequestBase
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AppId { get; set; } = "";
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FormId { get; set; } = "";
+
+        /// <summary>
+        /// 推送地址
+        /// </summary>
+        public string Url { get; set; } = "";
+        /// <summary>
+        /// Secret
+        /// </summary>
+        public string Secret { get; set; } = "";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public WebHookTrigger Triggers { get; set; }
     }
 }
 
--- EIMSNext/Service/EIMSNext.Entity/Webhook.cs
+++ EIMSNext/Service/EIMSNext.Entity/Webhook.cs
@@ -21,5 +21,51 @@ namespace EIMSNext.Entity
         /// 
         /// </summary>
         public string FormId { get; set; } = "";
+
+        /// <summary>
+        /// 推送地址
+        /// </summary>
+        public string Url { get; set; } = "";
+        /// <summary>
+        /// Secret
+        /// </summary>
+        public string Secret { get; set; } = "";
+
+        /// <summary>
+        /// WebHook触发模式，类型为WebHookTrigger
+        /// </summary>
+        public long Triggers { get; set; }
+    }
+
+    /// <summary>
+    /// WebHook触发模式
+    /// </summary>
+    [Flags]
+    public enum WebHookTrigger
+    {
+        /// <summary>
+        /// 未设置
+        /// </summary>
+        NotSet = 0,
+        /// <summary>
+        /// 数据新增
+        /// </summary>
+        DataCreated = 1 << 0,
+        /// <summary>
+        /// 数据修改
+        /// </summary>
+        DataUpdated = 1 << 1,
+        /// <summary>
+        /// 数据删除
+        /// </summary>
+        DateRemoved = 1 << 2,
+        /// <summary>
+        /// 流程状态变更
+        /// </summary>
+        WfStatusUpdated = 1 << 3,
+        /// <summary>
+        /// 流程待办变更
+        /// </summary>
+        WfTodoUpdated = 1 << 4,
     }
 }
