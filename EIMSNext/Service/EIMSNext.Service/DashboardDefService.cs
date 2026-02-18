17d2420033a6f0882c923ddc7e7d9c1babd421eb
Liwt <19781209@qq.com>
2026-02-18 22:16:54 +0800

    fix

--- EIMSNext/Service/EIMSNext.Service/DashboardDefService.cs
+++ EIMSNext/Service/EIMSNext.Service/DashboardDefService.cs
@@ -2,10 +2,27 @@
 using EIMSNext.Core.Service;
 using EIMSNext.Entity;
 using EIMSNext.Service.Interface;
+using EIMSNext.Core;
+using MongoDB.Driver;
 
 namespace EIMSNext.Service
 {
 	public class DashboardDefService(IResolver resolver) : EntityServiceBase<DashboardDef>(resolver), IDashboardDefService
 	{
-	}
+        protected override async Task AfterAdd(IEnumerable<DashboardDef> entities, IClientSessionHandle? session)
+        {
+            await base.AfterAdd(entities, session);
+            var appRepo = Resolver.GetRepository<App>();
+            var app = appRepo.Get(entities.First().AppId, session)!;
+            var maxIndex = app.AppMenus.Count == 0 ? 0 : app.AppMenus.Max(x => x.SortIndex);
+            entities.ForEach(e =>
+            {
+                maxIndex = maxIndex + 100;
+                app.AppMenus.Add(new AppMenu { MenuId = e.Id, Icon = "", IconColor = "", MenuType = FormType.Dashboard, Title = e.Name, SortIndex = maxIndex });
+            });
+            appRepo.Replace(app, session);
+
+            return;
+        }
+    }
 }
