3a09730ce9beee75c10f72ad56be3004f25392a1
Liwt <19781209@qq.com>
2026-06-29 17:09:34 +0800

    fix

--- EIMSNext/Auth/EIMSNext.Auth.Entities/InternalClients.cs
+++ EIMSNext/Auth/EIMSNext.Auth.Entities/InternalClients.cs
@@ -3,7 +3,7 @@ namespace EIMSNext.Auth.Entities
     public static class InternalClients
     {
         public const string WebClientId = "EIMSD160507G0401S171201B1014Next";
-        public const string SystemClientId = "EIMSNext.System";
+        public const string SystemClientId = "eimsnext.system";
         public const string SystemClientSecret = "G@wT!yU#iO$pA%sD^f&J*HkLzXcVbNmQ";
     }
 }
